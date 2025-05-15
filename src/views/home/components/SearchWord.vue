93d89943b949032427c69ab747798fe4a469b01f
yxt001 <99328846+zhonwen@users.noreply.github.com>
2025-05-15 15:59:14 +0800

    更新页面跳转路径 (#43)
    Co-authored-by: yantao <yantao@zcunsoft.com>

--- src/views/home/components/SearchWord.vue
+++ src/views/home/components/SearchWord.vue
@@ -2,7 +2,7 @@
   <div class="SearchWord block-main public-hoverItem">
     <div
       class="block-head"
-      @click="$router.push('/visitorAnalysis/search')"
+      @click="$router.push('/access/search')"
     >
       <div class="block-title">Top10搜索词</div>
       <div class="block-head-icon">
