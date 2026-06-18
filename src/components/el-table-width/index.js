0413fa1dd2fa0beb3b6261954533565e8a5024ec
yantao <yantao@zcunsoft.com>
2026-06-18 11:50:31 +0800

    feat:增加密钥管理

---
+++ src/components/el-table-width/index.js
@@ -0,0 +1,10 @@
+export const fixedWidth = {
+  FwMulti: "50px", //多选
+  FwIndex: "60px", //序号
+  FwValue: "80px", //占比
+  FwDate: "150px", //年月日
+  FwEventName: "150px", //事件名称
+  FwTime: "160px", //年月日时分秒
+  FwDisplayName: "180px", //显示名
+  FwOperation: "150px", //操作列
+};
