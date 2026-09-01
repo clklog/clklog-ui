76c792bb7c3769f60075b004f87dcefed82b0311
yantao <yantao@zcunsoft.com>
2026-09-01 10:58:05 +0800

    fit:优化重定向规则跳转

--- src/permission.js
+++ src/permission.js
@@ -48,23 +48,31 @@ router.beforeEach(async(to, from, next) => {
           // remove token and go to login page to re-login
           await store.dispatch('user/resetToken')
           Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
+          // NoPermission 是错误提示页，不适合作为登录后的回跳目标
+          const loginPath = to.path === '/NoPermission'
+            ? '/login'
+            : `/login?redirect=${to.path}`
+          next(loginPath)
           NProgress.done()
         }
       }
     }
-  } else {
-    /* has no token*/
-
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
-      next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+      /* has no token*/
+
+      if (whiteList.indexOf(to.path) !== -1) {
+        // in the free login whitelist, go directly
+        next()
+      } else {
+        // other pages that do not have permission to access are redirected to the login page.
+        // NoPermission 是错误提示页，不适合作为登录后的回跳目标
+        const loginPath = to.path === '/NoPermission'
+          ? '/login'
+          : `/login?redirect=${to.path}`
+        next(loginPath)
+        NProgress.done()
+      }
     }
-  }
 })
 
 router.afterEach(() => {
